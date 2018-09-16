6c839ca9296a9041ea5acc2312772879c791c2f0
= <brewcrew5498@outlook.com>
2018-09-16 19:02:31 -0400

    Read from stdin instead of from file

--- dna.py
+++ dna.py
@@ -1,12 +1,6 @@
-#!/usr/bin/env python3.6
 from collections import Counter
 
-FILE = 'datasets/dna.txt'
 ELEMENTS = 'ACGT'
 
-def main():
-    counter = Counter(f.read())
-    print(' '.join(str(counter[element]) for element in ELEMENTS))
-
-if __name__ == '__main__':
-  main(input())
+counter = Counter(input())
+print(' '.join(str(counter[element]) for element in ELEMENTS))
--- rna.py
+++ rna.py
@@ -1,10 +1,4 @@
 #!/usr/bin/env python3.6
 
-FILE = 'datasets/rna.txt'
+print(''.join('U' if element == 'T' else element for element in input()))
 
-def main():
-  with open(FILE, 'r') as f:
-    print(''.join('U' if element == 'T' else element for element in f.read()))
-
-if __name__ == '__main__':
-  main()
