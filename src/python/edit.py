c59930d3341e17d9f9f29c3b6c39d44f845d3215
= <brewcrew5498@outlook.com>
2020-06-06 20:34:32 -0500

    edit

--- src/python/edit.py
+++ src/python/edit.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3.6
 import argparse
+import numpy as np
 
 from utils import fasta
 
@@ -11,18 +12,17 @@ if __name__ == '__main__':
   assert len(words) == 2, len(words)
 
   A, B = words
+  print(A, B)
   n = len(A)
   m = len(B)
-  E = np.zeros((n, m), dtype=np.int32)
-  for i in range(n):
-    for j in range(m):
+  E = np.zeros((n+1, m+1), dtype=np.int32)
+  for i in range(n+1):
+    for j in range(m+1):
       if i == j == 0: continue
-      E[i, j] = np.min(
-        1 + E(i, j-1) if j > 0 else np.inf,
-        1 + E(i-1, j) if i > 0 else np.inf,
-        int(A[i] == B[j]) + E(i-1, j-1) if i > 0 and j > 0 else np.inf
+      E[i, j] = min(
+        1 + E[i, j-1] if j > 0 else np.inf,
+        1 + E[i-1, j] if i > 0 else np.inf,
+        (int(A[i-1] != B[j-1]) + E[i-1, j-1]) if i > 0 and j > 0 else np.inf
       )
   print(E)
-  print(E[n-1,m-1])
-
-
+  print(E[n,m])
